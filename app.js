d8cc29446e569088d276215e2076cdaeba2031b4
homeristico <homeristico@hotmail.com>
2023-07-09 19:40:39 -0500

    port chaged to 443

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/saludo', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 443;
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
 });
